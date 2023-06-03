112e6943111a51bebf5a29e9086abbcd3d6a7317
VadikRakitin01 <vadym.rakitin@gmail.com>
2023-06-03 19:13:18 +0300

    Initial commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,12 +6,15 @@ import "./navbar.css";
 export const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="links">
-        <Link to="/"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
-        <Link to="/cart">
+      <div className="links"> 
+        <p>AppleShop</p>
+        <div>
+          <Link to="/"> Shop </Link>
+          <Link to="/cart">
           <ShoppingCart size={32} />
         </Link>
+        </div>
+        
       </div>
     </div>
   );
